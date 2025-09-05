fccd54ff7e236b0cb7a42ddede093af665597fde
surunxian <surunxian@126.com>
2025-09-05 20:58:38 +0800

    更新 Lock.swift

--- Intents/Lock.swift
+++ Intents/Lock.swift
@@ -11,13 +11,15 @@ struct LockAppIntent: AppIntent {
 	)
 
 	func perform() async throws -> some IntentResult {
-		let handle = dlopen(FW_ManagedConfiguration, RTLD_LAZY)
-		if handle != nil {
-            MCProfileConnection.shared().lockDevice()
-			dlclose(handle)
-		} else {
-            throw NSError(domain: "TrollCuts", code: 99, userInfo: [NSLocalizedDescriptionKey: String(format: "Failed to load %@", FW_ManagedConfiguration)] )
+		        if let connection = MCProfileConnection.shared() {
+            connection.lockDevice()
+        } else {
+            throw NSError(
+                domain: "TrollCuts",
+                code: 99,
+                userInfo: [NSLocalizedDescriptionKey: "Failed to access MCProfileConnection"]
+            )
         }
-		return .result()
-	}
+        return .result()
+    }
 }
